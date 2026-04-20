bb37bb34d0de6c61c733e01a67239981cdc405fb
ejesseman <lizzyjesseman@hotmail.com>
2026-04-20 16:23:32 -0400

    Created login

--- robot-delivery-app/backend/config/urls.py
+++ robot-delivery-app/backend/config/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from apps.views import hello_world
+from apps.views import login_view
 
 
 
@@ -13,6 +14,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/hello/', hello_world),
+
+    path('login/', login_view),
 ]
 
 
