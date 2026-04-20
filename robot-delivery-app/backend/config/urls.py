085e2c03a0c784a6d2fd510a434123f9cc8797bb
AlphaXFighter <nnguy5@gmu.edu>
2026-04-20 16:17:11 -0400

    testing

--- robot-delivery-app/backend/config/urls.py
+++ robot-delivery-app/backend/config/urls.py
@@ -1,12 +1,10 @@
 # """Root URL Configuration"""
 # from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.urls import path
+from . import views
 
 api_v1 = [
-
+    path('config/', views.config, name='config'),
 ]
 
 
