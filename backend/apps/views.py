a10af2050729627ece8990703a35dd340a9ac1a2
ejesseman <lizzyjesseman@hotmail.com>
2026-04-23 13:15:53 -0400

    Made some fixes

--- backend/apps/views.py
+++ backend/apps/views.py
@@ -7,7 +7,7 @@ from django.http import JsonResponse
 from rest_framework import status
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .models import Order
+from apps.orders import Order
 from django.contrib.auth.hashers import check_password
 
 @api_view(['GET', 'POST'])
@@ -48,13 +48,6 @@ class LoginView(APIView):
                     {"error": "Wrong password"},
                     status=status.HTTP_401_UNAUTHORIZED
                 )
-
-@login_required
-def order_history(request):
-    # Filter orders for the logged-in user [3]
-    orders = Order.objects.filter(user=request.user).order_by('-created_at')
-    context = {'orders': orders}
-    return render(request, 'order_history.html', context)
         except User.DoesNotExist:
 
             print("USER NOT FOUND")
@@ -64,5 +57,12 @@ def order_history(request):
                 status=status.HTTP_404_NOT_FOUND
             )
 
+@login_required
+def order_history(request):
+    # Filter orders for the logged-in user [3]
+    orders = Order.objects.filter(user=request.user).order_by('-created_at')
+    context = {'orders': orders}
+    return render(request, 'order_history.html', context)
+        
     
     
