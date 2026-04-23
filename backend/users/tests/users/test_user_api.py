0507cc32544a75761c518117a84f0841724e6517
Jesse Sorman <jessesorman@gmail.com>
2026-04-23 16:53:49 -0400

    add test user login test

--- backend/users/tests/users/test_user_api.py
+++ backend/users/tests/users/test_user_api.py
@@ -1,10 +1,15 @@
 import pytest
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 @pytest.mark.django_db
 def test_user_login_endpoint(api_client, user):
-  url = reverse('/api/users/login')
+  url = reverse('login')
   data = {"username": "johndoe", "password": "psswrd123!"}
   response = api_client.post(url, data, format='json')
+
+  if response.status_code != 200:
+    print(response.data) 
 
   assert response.status_code == 200
