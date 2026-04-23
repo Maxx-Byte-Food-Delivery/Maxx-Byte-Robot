1aabed93b61f1ec3e70e0a77bf9743d9691be02e
Pro Phoenix <julian.jy.lee100@gmail.com>
2026-04-23 14:57:30 -0400

    Resolve merge conflict

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -7,7 +7,7 @@ from django.contrib import admin
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from apps.views import hello_world
 from apps.views import login_view
-
+from apps.views import get_all_products
 
 
 urlpatterns = [ 
@@ -16,6 +16,8 @@ urlpatterns = [
     path('api/hello/', hello_world),
 
     path('login/', login_view),
+
+    path('api/all_products/', get_all_products)
 ]
 
 
