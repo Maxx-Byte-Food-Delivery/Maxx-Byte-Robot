50ffca09bb7a80384fb172600964f929141696d2
Pro Phoenix <julian.jy.lee100@gmail.com>
2026-04-24 15:39:47 -0400

    Resolve merge conflicts

--- backend/apps/urls.py
+++ backend/apps/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from views.hello_world import hello_world
 from views.login import LoginView
+from views.order_history import order_history
 
 urlpatterns = [
     path('users/login/', LoginView.as_view(), name='login'),
     path('hello/', hello_world),
-    # path('users/order_history/', OrderHistory.as_view())
+    path('order_history/', order_history)
 ]
--- backend/views/hello_world.py
+++ backend/views/hello_world.py
@@ -1,13 +1,5 @@
-from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.contrib.auth import authenticate, login
-from django.http import JsonResponse
-from rest_framework import status
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from apps.models.order import Order
-from django.contrib.auth.hashers import check_password
 
 @api_view(['GET', 'POST'])
 
