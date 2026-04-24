7899ca8c7b75df391b2bdd1cbbc13051ff33712e
Jesse Sorman <jessesorman@gmail.com>
2026-04-24 16:46:00 -0400

    fix conflicts

--- backend/apps/urls.py
+++ backend/apps/urls.py
@@ -1,18 +1,9 @@
 from django.urls import path
 from apps.views.hello_world import hello_world
 from apps.views.login import LoginView
-from apps.views.order_history import OrderHistory
-from apps.views.payment import create_checkout_session, stripe_webhook
 
 urlpatterns = [
     path('users/login/', LoginView.as_view(), name='login'),
     path('hello/', hello_world),
-
-    path('order_history/', OrderHistory.as_view()),
-
-    path('checkout/<int:order_id', create_checkout_session),
-    
-    path('stripe/webhook', stripe_webhook),
-
     # path('users/order_history/', OrderHistory.as_view())
 ]
