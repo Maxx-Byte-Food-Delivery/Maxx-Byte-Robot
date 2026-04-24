bea512f1809fc4e924f26275d3b05bbef7c4f0c7
gelseybostick <gelseybostick@gmail.com>
2026-04-24 15:25:47 -0400

    Completed payment model, added views for checkout and payment confirmation, and added checkout and stripe webhook to urlpatterns.

--- backend/apps/urls.py
+++ backend/apps/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from views.hello_world import hello_world
 from views.login import LoginView
 from views.order_history import OrderHistory
+from views.payment import create_checkout_session, stripe_webhook
 
 urlpatterns = [
 
@@ -9,6 +10,10 @@ urlpatterns = [
 
     path('hello/', hello_world),
 
-    path('order_history/', OrderHistory.as_view())
+    path('order_history/', OrderHistory.as_view()),
+
+    path('checkout/<int:order_id', create_checkout_session),
+    
+    path('stripe/webhook', stripe_webhook),
 
 ]
