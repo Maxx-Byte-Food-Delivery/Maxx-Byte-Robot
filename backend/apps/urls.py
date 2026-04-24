01d0b81266dfaa157088ed6a9848733ecb68d5b7
Jesse Sorman <jessesorman@gmail.com>
2026-04-24 15:10:41 -0400

    fix merge issues

--- backend/apps/urls.py
+++ backend/apps/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from views.hello import hello_world
+from views.hello_world import hello_world
 from views.login import LoginView
 
 urlpatterns = [
     path('users/login/', LoginView.as_view(), name='login'),
-    path('hello/', hello_world)
-    path('users/order_history/', OrderHistory.as_view())
+    path('hello/', hello_world),
+    # path('users/order_history/', OrderHistory.as_view())
 ]
--- backend/views/login.py
+++ backend/views/login.py
@@ -7,10 +7,8 @@ from django.http import JsonResponse
 from rest_framework import status
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from apps.orders import Order
+from apps.models.order import Order
 from django.contrib.auth.hashers import check_password
-
-@api_view(['GET', 'POST'])
 
 class LoginView(APIView):
 
