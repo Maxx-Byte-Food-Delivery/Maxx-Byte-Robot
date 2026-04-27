8876e59349b3bba409c4ee6053500ad85b96b458
Pro Phoenix <julian.jy.lee100@gmail.com>
2026-04-27 15:04:28 -0400

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,13 +2,11 @@
 # from django.contrib import admin
 from django.urls import path, include
 from django.contrib import admin
-from views.login import LoginView
 from views.products import get_all_products
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("apps.urls")),
-    path('login/', LoginView.as_view()),
     path('api/all_products/', get_all_products)
 ]
 
