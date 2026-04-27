957e86c3f1df97c1a47ab6bbc9c53d575620a4e9
gelseybostick <gelseybostick@gmail.com>
2026-04-27 14:14:30 -0400

    url changes

--- backend/apps/urls.py
+++ backend/apps/urls.py
@@ -1,16 +1,12 @@
 from django.urls import path
 from views.hello_world import hello_world
 from views.login import LoginView
-<<<<<<< HEAD
 from views.order_history import OrderHistory
 from views.payment import create_checkout_session, stripe_webhook
-=======
->>>>>>> de720c3edb56c8ad6b46383eb4caaa6b0e34d027
 
 urlpatterns = [
     path('users/login/', LoginView.as_view(), name='login'),
     path('hello/', hello_world),
-<<<<<<< HEAD
 
     path('order_history/', OrderHistory.as_view()),
 
@@ -18,7 +14,4 @@ urlpatterns = [
     
     path('stripe/webhook', stripe_webhook),
 
-=======
-    # path('users/order_history/', OrderHistory.as_view())
->>>>>>> de720c3edb56c8ad6b46383eb4caaa6b0e34d027
 ]
