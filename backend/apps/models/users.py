37da9bd97e6017ad3ae3dcc0c8ec2230bfa0111d
ejesseman <lizzyjesseman@hotmail.com>
2026-04-27 13:33:48 -0400

    Added user type in users.py

--- backend/apps/models/users.py
+++ backend/apps/models/users.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 class User(models.Model):
+    
+    USER_TYPE_CHOICES = (   ##Include user type (staff/student)
+        ("staff", "Staff"),
+        ("student", "Student"),
+    )
     username = models.CharField(max_length=150, unique=True)
     password = models.CharField(max_length=255)
     email = models.EmailField()
