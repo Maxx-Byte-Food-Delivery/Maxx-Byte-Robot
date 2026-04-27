8d4ed9e374fbc23fef43cb581db0a9faad1280af
Pro Phoenix <julian.jy.lee100@gmail.com>
2026-04-27 14:45:33 -0400

    Fix build issues

--- backend/apps/views/hello_world.py
+++ backend/apps/views/hello_world.py
@@ -1,10 +1,5 @@
-
-from rest_framework.views import APIView
 from rest_framework.decorators import api_view
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from apps.models.order import Order
-from django.contrib.auth.hashers import check_password
+from rest_framework.response import Response
 
 @api_view(['GET', 'POST'])
 
--- backend/views/login.py
+++ backend/views/login.py
@@ -1,4 +1,5 @@
-from backend.apps.models.users import User
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
