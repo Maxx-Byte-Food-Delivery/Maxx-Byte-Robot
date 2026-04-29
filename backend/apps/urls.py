5f2e555e23bfa61de5a3f68028d33f9c63dbf0dd
AlphaXFighter <nnguy5@gmu.edu>
2026-04-29 15:06:13 -0400

    Add Order History Resolved

--- backend/apps/urls.py
+++ backend/apps/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 from apps.views.hello_world import hello_world
 from apps.views.login import LoginView
 from apps.views.payment import create_checkout_session, stripe_webhook
+from apps.views.order_history import View_History, item, reorder
 
 
 urlpatterns = [
     path('users/login/', LoginView.as_view(), name='login'),
     path('hello/', hello_world),
     path('checkout/<int:order_id>/', create_checkout_session, name='create_checkout_session'),
-    path('stripe/webhook', stripe_webhook)
+    path('stripe/webhook', stripe_webhook),
+    path('View_History/', View_History, name='View_History'),
+    path('View_History/item/<int:id>/', item, name='item'),
+    path('reorder/', reorder, name='reorder'),
 ]
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -162,7 +162,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.admin',
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
