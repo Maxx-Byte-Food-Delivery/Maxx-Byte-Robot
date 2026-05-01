fb72db75061421c8070a3802f4e46578d23e85e8
Jesse Sorman <jessesorman@gmail.com>
2026-05-01 15:44:15 -0400

    add order tracking api endpoint tests

---
+++ backend/robot_delivery/tests/api/orders/test_order_tracking.py
@@ -0,0 +1,34 @@
+import pytest
+from django.urls import reverse
+
+# when unskipping these tests change @pytest.mark.skip to @pytest.mark.django_db
+# make sure url = reverse('place_order') matches the name of the url pattern for the order placement endpoint in the urls.py file
+
+#user can place an order successfully
+@pytest.mark.skip(reason="order tracking endpoint not yet implemented")
+def test_order_tracking_api_endpoint(api_client, users, create_orders):
+  api_client.force_authenticate(user=users[0])
+
+  url = reverse('order_tracking', kwargs={'order_id': create_orders[0].id, 'user_id': users[0].id})
+  response = api_client.get(url)
+
+  assert response.status_code == 200
+  assert response.data['status'] == "pending"
+  assert response.data['eta'] == "10 minutes"
+  assert response.data['destination'] == "2 miles"
+
+@pytest.mark.skip(reason="order tracking endpoint not yet implemented")
+def test_order_tracking_api_endpoint_unauthenticated(api_client, create_orders):
+  url = reverse('order_tracking', kwargs={'order_id': create_orders[0].id})
+  response = api_client.get(url)
+
+  assert response.status_code == 403
+  assert response.data['detail'] == "This request requires you to be logged in."
+
+@pytest.mark.skip(reason="order tracking endpoint not yet implemented")
+def test_order_tracking_api_endpoint_no_order(api_client, create_orders):
+  url = reverse('order_tracking', kwargs={'user_id': users[0].id})
+  response = api_client.get(url)
+
+  assert response.status_code == 403
+  assert response.data['detail'] == "This request requires you to be logged in."
