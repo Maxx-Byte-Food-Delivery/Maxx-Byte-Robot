4a131829215d23ca0cb124463932d3905b36a791
Jesse Sorman <jessesorman@gmail.com>
2026-05-06 16:09:53 -0400

    add login as admin test

--- backend/robot_delivery/tests/api/users/test_user_api.py
+++ backend/robot_delivery/tests/api/users/test_user_api.py
@@ -36,6 +36,15 @@ def test_user_logout_endpoint(api_client, users):
   assert response.data['message'] == "Logged out successfully"
 
 @pytest.mark.django_db
+def test_user_logout_as_admin(api_client, admin_users):
+  api_client.force_authenticate(user=admin_users[0])
+  url = reverse('logout')
+  response = api_client.post(url, format='json')
+
+  assert response.status_code == 200
+  assert response.data['message'] == "Logged out successfully"
+
+@pytest.mark.django_db
 def test_user_logout_endpoint_not_logged_in(api_client, users):
   url = reverse('logout')
   response = api_client.post(url, format='json')
