43115fd9961e9b3ed3322e5ab68874ae1abb96c6
ejesseman <lizzyjesseman@hotmail.com>
2026-05-07 15:19:45 -0400

    Deleted some things

--- backend/apps/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from ..orders.models.order import Order
-from ..orders.models.order_item import OrderItem
-from ..payments.models.payment import Payment
-
-__all__ = ['Order', 'OrderItem', 'Payment']
