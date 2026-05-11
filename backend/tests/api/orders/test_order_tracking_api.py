72776a45ed0db4fe5524f6e51f53122e7f3acbf9
Jesse Sorman <jessesorman@gmail.com>
2026-05-11 16:55:49 -0400

    refactor order tracking tests

--- backend/tests/api/orders/test_order_tracking_api.py
+++ backend/tests/api/orders/test_order_tracking_api.py
@@ -8,40 +8,47 @@ from django.urls import reverse
 @pytest.mark.django_db
 def test_order_tracking_api_endpoint(api_client, users, create_orders):
   api_client.force_authenticate(user=users[0])
-
-  url = reverse('active_order', args=[create_orders[0].id, users[0].id])
   create_orders[0].status = "active"
+  create_orders[0].save()
+  url = reverse('active_order', args=[create_orders[0].id, users[0].id])
+  
   response = api_client.get(url)
 
   assert response.status_code == 200
-  assert response.data['status'] == "active"
-  assert response.data['eta'] == "10 minutes"
-  assert response.data['destination'] == "2 miles"
+  assert response.data[0]['status'] == "active"
+  assert response.data[0]['id'] == create_orders[0].id
+  assert float(response.data[0]['total_price']) == float(39.98)
+  # assert response.data['eta'] == "10 minutes"
+  # assert response.data['destination'] == "2 miles"
 
 @pytest.mark.django_db
 def test_order_tracking_api_endpoint_unauthenticated(api_client, create_orders, users):
-  url = reverse('active_order', args=[create_orders[0].id])
   create_orders[0].status = "active"
+  create_orders[0].save()
+
+  url = reverse('active_order', args=[create_orders[0].id, users[0].id])
   response = api_client.get(url)
   
   assert response.status_code == 403
-  assert response.data['detail'] == "This request requires you to be logged in."
+  assert response.data['detail'] == "You must be logged in to track an order"
 
 @pytest.mark.django_db
 def test_order_tracking_api_endpoint_wrong_user(api_client, create_orders, users):
   api_client.force_authenticate(user=users[0])
-
-  url = reverse('active_order', args=[create_orders[0].id, users[1].id])
-  response = api_client.get(url)
   create_orders[0].status = "active"
+  create_orders[0].save()
+
+  url = reverse('active_order', args=[users[1].id, create_orders[0].id])
+  response = api_client.get(url)
 
   assert response.status_code == 403
   assert response.data['error'] == "Unauthorized"
 
 @pytest.mark.django_db
 def test_order_tracking_api_endpoint_no_order(api_client, create_orders, users):
-  url = reverse('active_order', args=[users[1].id])
+  api_client.force_authenticate(user=users[0])
+  url = reverse('active_order', args=[users[0].id, 9999])
   response = api_client.get(url)
 
-  assert response.status_code == 400
-  assert response.data['error'] == "This request requires an order"
+  assert response.status_code == 404
+  assert response.data['error'] == "No orders found"
