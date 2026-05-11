44a7e9315ec4ad1bd283ec63c574776e1680c821
Jesse Sorman <jessesorman@gmail.com>
2026-05-11 14:06:30 -0400

    add weak password test to change password tests

--- backend/tests/api/users/test_user_change_password_api.py
+++ backend/tests/api/users/test_user_change_password_api.py
@@ -5,18 +5,29 @@ from django.urls import reverse
 # make sure url = reverse('change_password') matches the name of the url pattern for the user change password endpoint in the urls.py file
 
 @pytest.mark.skip(reason = "change password endpoint not yet implamented")
-def test_user_chnage_password_endpoint(api_client, users):
+def test_user_change_password_endpoint(api_client, users):
   api_client.force_authenticate(user=users[0])
   url = reverse('change_password')
-  data = {"old_password": "psswrd123!", "new_password": "newpsswrd123!"}
+  data = {"old_password": "VeryG00d!Password", "new_password": "Som3NewG00dP!assword"}
   response = api_client.post(url, data, format='json')
 
   assert response.status_code == 200
   assert response.data['message'] == "password changed successfully"
-  assert get_user_model().objects.get(username="johndoe").check_password("newpsswrd123!") == True
+  assert get_user_model().objects.get(username="johndoe").check_password("Som3NewG00dP!assword") == True
 
 @pytest.mark.skip(reason = "change password endpoint not yet implamented")
-def test_user_chnage_password_endpoint_not_logged_in(api_client, users):
+def test_user_change_password_endpoint_weak_password(api_client, users):
+  api_client.force_authenticate(user=users[0])
+  
+  with pytest.raises(ValidationError) as excinfo:
+    url = reverse('change_password')
+    data = {"old_password": "VeryG00d!Password", "new_password": "password"}
+    response = api_client.post(url, data, format='json')
+  assert "This password is too common." in str(excinfo.value)
+  
+
+@pytest.mark.skip(reason = "change password endpoint not yet implamented")
+def test_user_change_password_endpoint_not_logged_in(api_client, users):
   url = reverse('change_password')
   data = {"old_password": "psswrd123!", "new_password": "newpsswrd123!"}
   response = api_client.post(url, data, format='json')
