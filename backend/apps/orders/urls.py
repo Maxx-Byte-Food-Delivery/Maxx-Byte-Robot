f0611db57e4fc51a40af3f97391a987dd307b396
AlphaXFighter <nnguy5@gmu.edu>
2026-05-11 15:16:46 -0400

    Ative Order URL

--- backend/apps/orders/urls.py
+++ backend/apps/orders/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path;
 from apps.orders.views.order_history import View_History, item, reorder
+from apps.orders.views.active_order import ActiveOrderView
 
 urlpatterns = [
     path('users/<int:user_id>/orders/view_history/', View_History, name='view_history'),
     path('users/<int:user_id>/orders/view_history/item/<int:id>/', item, name='view_history_item'),
     path('users/<int:user_id>/orders/reorder/<int:id>/', reorder, name='reorder'),
+    path('users/<int:user_id>/orders/active_order/<int:id>/', ActiveOrderView, name= 'active_order'),
 ]
