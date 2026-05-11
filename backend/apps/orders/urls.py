d7910d79645496cf963085060e9b6feeab0a9359
AlphaXFighter <nnguy5@gmu.edu>
2026-05-11 16:25:08 -0400

    Refracter URL

--- backend/apps/orders/urls.py
+++ backend/apps/orders/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('users/<int:user_id>/orders/view_history/', View_History, name='view_history'),
     path('users/<int:user_id>/orders/view_history/item/<int:id>/', item, name='view_history_item'),
     path('users/<int:user_id>/orders/reorder/<int:id>/', reorder, name='reorder'),
-    path('users/<int:user_id>/orders/active_order/<int:id>/', ActiveOrderView, name= 'active_order'),
+    
+    path('users/<int:user_id>/active-orders/', ActiveOrderView.as_view(), name='active_order_list'),
+    path('users/<int:user_id>/active-orders/<int:order_id>/', ActiveOrderView.as_view(), name='active_order'),
 ]
