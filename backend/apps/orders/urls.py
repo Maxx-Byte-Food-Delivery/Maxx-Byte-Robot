f4d0d335c804f7d202bf5b245988c368da2b99ab
Jesse Sorman <jessesorman@gmail.com>
2026-05-11 16:56:20 -0400

    add error exceptions to order tracking

--- backend/apps/orders/urls.py
+++ backend/apps/orders/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path;
 from apps.orders.views.order_history import View_History, item, reorder
-from apps.orders.views.active_order import ActiveOrderView
+from apps.orders.views.active_order import active_order
 
 urlpatterns = [
     path('users/<int:user_id>/orders/view_history/', View_History, name='view_history'),
     path('users/<int:user_id>/orders/view_history/item/<int:id>/', item, name='view_history_item'),
     path('users/<int:user_id>/orders/reorder/<int:id>/', reorder, name='reorder'),
-    
-    path('users/<int:user_id>/active-orders/', ActiveOrderView.as_view(), name='active_order_list'),
-    path('users/<int:user_id>/active-orders/<int:order_id>/', ActiveOrderView.as_view(), name='active_order'),
+    path('users/<int:user_id>/active-orders/<int:order_id>/', active_order, name='active_order'),
 ]
--- backend/apps/orders/views/active_order.py
+++ backend/apps/orders/views/active_order.py
@@ -1,39 +1,42 @@
-from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from apps.orders.models import Order
 from apps.serializers.active_order import ActiveOrderSerializer
 
+@api_view(['GET'])
+def active_order(request, user_id, order_id):
+    if not request.user.is_authenticated:
+        return Response({'detail': 'You must be logged in to track an order', 'orders': []}, status=status.HTTP_403_FORBIDDEN)
+    elif request.user.id != user_id:
+        return Response({'error': 'Unauthorized'}, status=status.HTTP_403_FORBIDDEN)
+        
+    orders = Order.objects.filter(user_id=user_id, status = "active")
+    if not orders.exists():
+        return Response({'error': 'No orders found', 'orders': []}, status=status.HTTP_404_NOT_FOUND)
+    orders = orders.distinct()
 
-class ActiveOrderView(APIView):
-    # GET: List all active orders
-    def get(self, request, user_id,order_id):
-        try:
-            order = Order.objects.get(id = order_id, user_id = user_id, status = "active")
+    order_list = list(orders.values('id', 'total_price', 'status', 'user', 'created_at'))
 
-            serializer = ActiveOrderSerializer(order)
+    return Response(order_list)
+
+
+
+
+def patch(self, request, user_id, order_id):
+    try:
+        order = Order.objects.get(
+            id=order_id,
+            user_id=user_id
+        )
+
+        serializer = ActiveOrderSerializer(order, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
             return Response(serializer.data)
 
-        except Order.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    
-    
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def patch(self, request, user_id, order_id):
-        try:
-            order = Order.objects.get(
-                id=order_id,
-                user_id=user_id
-            )
-
-            serializer = ActiveOrderSerializer(order, data=request.data, partial=True)
-
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data)
-
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        except Order.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+    except Order.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
