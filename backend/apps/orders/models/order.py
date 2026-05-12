fbc855ea6f730d7c7bf0b4c97a1ea2fde0a2bf6e
Jesse Sorman <jessesorman@gmail.com>
2026-05-12 17:17:03 -0400

    add automatic price updating to order model

--- backend/apps/orders/models/order.py
+++ backend/apps/orders/models/order.py
@@ -10,6 +10,12 @@ STATUS_CHOICES = [
 class Order(models.Model):
 
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True)
-    total_price = models.DecimalField(max_digits=8, decimal_places=2)
+    total_price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
     status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='pending')
     created_at = models.DateTimeField(auto_now_add=True)
+    from django.db import models
+
+    def update_total(self):
+        items = self.order_items.all()
+        self.total_price = sum(item.price for item in items)
+        self.save()
