ef135416f9339283acb2edfc9cb8e5a73c5f0bd7
Jesse Sorman <jessesorman@gmail.com>
2026-05-18 15:28:46 -0400

    add empty cart test

--- backend/tests/e2e/test_cart.py
+++ backend/tests/e2e/test_cart.py
@@ -20,8 +20,22 @@ def test_add_products_to_cart(page: Page, live_server, users, create_products):
   page.get_by_role("link", name=re.compile(r"\d+")).click()
   page.wait_for_load_state("networkidle")
 
-  expect(
-    page.locator("p", has_text=f"{create_products[0].name} x1")
-  ).to_be_visible()
+  expect(page.locator("p", has_text=f"{create_products[0].name} x1")).to_be_visible()
 
+@pytest.mark.django_db(transaction = True)
+def test_empty_cart(page: Page, live_server, users, create_products):
+  page.goto("http://localhost:5173/")
+  page.get_by_placeholder("Username").fill("johndoe")
+  page.get_by_placeholder("Password").fill("VeryG00d!Password")
+  page.get_by_role("button", name="Login").click()
+  
+  expect(page).to_have_url(re.compile(r"/student"))
+  page.wait_for_load_state("networkidle")
 
+  page.get_by_role("link", name="Products").click()
+  page.wait_for_load_state("networkidle")
+
+  page.get_by_role("link", name=re.compile(r"\d+")).click()
+  page.wait_for_load_state("networkidle")
+
+  expect(page.locator("p", has_text="Your cart is empty")).to_be_visible()
