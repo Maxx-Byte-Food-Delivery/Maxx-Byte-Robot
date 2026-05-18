44d304bdc7b5dd3e338aa44a1b18c77c6c2bdb48
Jesse Sorman <jessesorman@gmail.com>
2026-05-18 17:40:58 -0400

    unskip active orders playwright test

--- backend/tests/e2e/test_active_order.py
+++ backend/tests/e2e/test_active_order.py
@@ -2,9 +2,11 @@ import re
 from playwright.sync_api import Page, expect
 import pytest
 ## replace skip(reason="") with django_db(transaction = True)
-@pytest.mark.skip(reason="not yet implemented")
+@pytest.mark.django_db(transaction = True)
 def test_active_orders(page: Page, live_server, users, create_orders):
-  create_orders.status = "Active"
+  create_orders[0].status = "active"
+  create_orders[0].save()
+
   page.goto("http://localhost:5173/")
   page.get_by_placeholder("Username").fill("johndoe")
   page.get_by_placeholder("Password").fill("VeryG00d!Password")
@@ -12,11 +14,14 @@ def test_active_orders(page: Page, live_server, users, create_orders):
   
   expect(page).to_have_url(re.compile(r"/student"))
 
-  page.goto("http://localhost:5173/active_orders")
-
+  page.goto("http://localhost:5173/orders/active")
   page.wait_for_load_state("networkidle")
 
   expect(
-    page.locator("p", has_text=f"Status: Active")
+    page.locator("p", has_text=f"Status: active")
+  ).to_be_visible()
+
+  expect(
+    page.locator("h3", has_text=f"Order #{create_orders[0].id}")
   ).to_be_visible()
   
