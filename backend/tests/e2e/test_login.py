94a279fc25ba810c0fa93719994fec25eb1cb430
Jesse Sorman <jessesorman@gmail.com>
2026-05-18 14:55:26 -0400

    add login as admin with sms test

--- backend/tests/e2e/test_login.py
+++ backend/tests/e2e/test_login.py
@@ -14,7 +14,7 @@ def test_login_as_user(page: Page, live_server, users):
   expect(page).to_have_url(re.compile(r"/student"))
 
 @pytest.mark.django_db(transaction = True)
-def test_login_as_admin(page: Page, live_server, admin_users, admin_profiles):
+def test_login_as_admin_totp(page: Page, live_server, admin_users, admin_profiles):
 
   page.goto("http://localhost:5173/")
 
@@ -37,3 +37,34 @@ def test_login_as_admin(page: Page, live_server, admin_users, admin_profiles):
   page.wait_for_load_state("networkidle")
 
   expect(page).to_have_url(re.compile(r"/staff"))
+
+@pytest.mark.django_db(transaction = True)
+def test_login_as_admin_sms(page: Page, live_server, admin_users, admin_profiles):
+
+  page.goto("http://localhost:5173/")
+
+  page.get_by_placeholder("Username").fill("admin2")
+  page.get_by_placeholder("Password").fill("AdminP@ssw0rd!")
+
+  page.get_by_role("button", name="Login").click()
+  page.wait_for_load_state("networkidle")
+
+  expect(page.get_by_text("Invalid username or password")).not_to_be_visible()
+
+  expect(page).to_have_url(re.compile(r"/verify-sms"))
+
+  target_user = admin_users[1]
+    
+  target_user.refresh_from_db()
+
+  fresh_sms_code = target_user.profile.mfa_code
+
+  data = {"code": fresh_sms_code}
+  
+  page.get_by_placeholder("6-digit code").fill(data["code"])
+
+  page.get_by_role("button", name="Verify").click()
+  page.wait_for_load_state("networkidle")
+
+  expect(page).to_have_url(re.compile(r"/staff"))
+
