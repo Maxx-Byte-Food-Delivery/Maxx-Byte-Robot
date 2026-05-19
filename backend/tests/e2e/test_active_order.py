4d470e937e5fc5d9f447457668535e12afc2e19c
Jesse Sorman <jessesorman@gmail.com>
2026-05-19 13:46:42 -0400

    add active order by id test

--- backend/tests/e2e/test_active_order.py
+++ backend/tests/e2e/test_active_order.py
@@ -24,4 +24,27 @@ def test_active_orders(page: Page, live_server, users, create_orders):
   expect(
     page.locator("h3", has_text=f"Order #{create_orders[0].id}")
   ).to_be_visible()
+
+@pytest.mark.skip(reason="not yet implemented")
+def test_active_order(page: Page, live_server, users, create_orders):
+  create_orders[0].status = "active"
+  create_orders[0].save()
+
+  page.goto("http://localhost:5173/")
+  page.get_by_placeholder("Username").fill("johndoe")
+  page.get_by_placeholder("Password").fill("VeryG00d!Password")
+  page.get_by_role("button", name="Login").click()
+  
+  expect(page).to_have_url(re.compile(r"/student"))
+
+  page.goto("http://localhost:5173/orders/active/{create_orders[0].id}")
+  page.wait_for_load_state("networkidle")
+
+  expect(
+    page.locator("p", has_text=f"Status: active")
+  ).to_be_visible()
+
+  expect(
+    page.locator("h3", has_text=f"Order #{create_orders[0].id}")
+  ).to_be_visible()
   
